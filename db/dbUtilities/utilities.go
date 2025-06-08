e199f24aa510322a8474fe94b7be4e8047b9af93
Glenn Sills <glennsills@gmail.com>
2025-06-08 15:36:53 -0400

    misc changes

--- db/dbUtilities/utilities.go
+++ db/dbUtilities/utilities.go
@@ -1,1 +1,25 @@
 package dbUtilities
+
+import (
+	"log"
+
+	"github.com/glennsills/yact/config"
+	usersData "github.com/glennsills/yact/db/usersData"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func MigrateDb() {
+	db, err := gorm.Open(postgres.Open(config.App.Dsn), &gorm.Config{})
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDB.Close()
+
+	err = db.AutoMigrate(&usersData.User{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
--- yact.go
+++ yact.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 
 	"github.com/glennsills/yact/config"
+	"github.com/glennsills/yact/db/dbUtilities"
 	"github.com/glennsills/yact/ui"
 )
 
 func main() {
 	config.InitAppConfig()
 	ui.AssignHandlers()
-
+	dbUtilities.MigrateDb()
 	srv := &http.Server{
 		Addr:    config.App.Addr,
 		Handler: ui.AssignHandlers(),
@@ -19,50 +20,3 @@ func main() {
 
 	log.Fatal(srv.ListenAndServe())
 }
-
-// func main() {
-
-// 	dsn := "host=database-1.c7im4k02oirc.us-east-2.rds.amazonaws.com dbname=yact port=5432 user=postgres password=SuperSecret dbname=yact sslmode=allow"
-// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	sqlDB, err := db.DB()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer sqlDB.Close()
-
-// 	err = db.AutoMigrate(&users_db.User{})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// Create a user
-// 	dob := time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC)
-// 	newUser := &users_db.User{
-// 		UserName:  "jdoe",
-// 		FirstName: "John",
-// 		LastName:  "Doe",
-// 		Email:     "john.doe@example.com",
-// 		Dob:       &dob,
-// 	}
-
-// 	repo := users_db.NewUserRepository(db)
-
-// 	err = repo.CreateUser(newUser)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	err = repo.DeleteUserById(newUser.ID)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	log.Printf("%v", newUser)
-
-// 	log.Default().Println("here")
-// }
